0fa07d75fbcd914fc3b4398308bbd7a18eee8d44
Vũ Văn Đặng <32575472+vuvandang1995@users.noreply.github.com>
2019-04-12 14:28:02 +0700

    Update tests.py

--- openid/superadmin/tests.py
+++ openid/superadmin/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
-
+from superadmin.models import *
 # Create your tests here.
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        MyUser.objects.create_user(username="dangvv", email="dangdiendao@gmail.com", password="123456")
+    
+    def check_username(self):
+        u = MyUser.objects.get(username="dangvv")
+        
+        self.asserEqual("dangvv", u.clean_username)
